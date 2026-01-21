a701a54d71b7c672cca8e78e2299ca7aa328cb3e
KeripikPepaya <234897641+KeripikPepaya@users.noreply.github.com>
2026-01-21 22:06:06 +0700

    Add new file .go simple game

---
+++ guessing_game.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+
+	//Generate a random number
+	target := random.Intn(10) + 1
+
+	fmt.Println("Wellcome to the Goessing Game!")
+	fmt.Println("I have chosen a number between 1 and 10")
+
+	var guess int
+
+	for {
+		fmt.Print("Enter your guess: ")
+		fmt.Scanln(&guess)
+
+		if guess == target {
+			fmt.Println("your guessing is correct")
+			break
+		} else if guess > target {
+			fmt.Println("your guess is to high")
+		} else {
+			fmt.Println("your guess is to low")
+		}
+	}
+}
